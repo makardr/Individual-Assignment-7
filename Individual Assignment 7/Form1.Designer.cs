bd990eb4821a54e807aa88a3b22125d963cd04ee
Makar <makardrozdik00@gmail.com>
2022-10-29 13:28:27 +0300

    Добавьте файлы проекта.

---
+++ Individual Assignment 7/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Individual_Assignment_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.correct_label = new System.Windows.Forms.Label();
+            this.incorrect_label = new System.Windows.Forms.Label();
+            this.passed_label = new System.Windows.Forms.Label();
+            this.calculate_btn = new System.Windows.Forms.Button();
+            this.incorrect_list_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // correct_label
+            // 
+            this.correct_label.AutoSize = true;
+            this.correct_label.Location = new System.Drawing.Point(115, 62);
+            this.correct_label.Name = "correct_label";
+            this.correct_label.Size = new System.Drawing.Size(124, 16);
+            this.correct_label.TabIndex = 1;
+            this.correct_label.Text = "Answered correctly:";
+            // 
+            // incorrect_label
+            // 
+            this.incorrect_label.AutoSize = true;
+            this.incorrect_label.Location = new System.Drawing.Point(115, 107);
+            this.incorrect_label.Name = "incorrect_label";
+            this.incorrect_label.Size = new System.Drawing.Size(134, 16);
+            this.incorrect_label.TabIndex = 2;
+            this.incorrect_label.Text = "Answered incorrectly:";
+            // 
+            // passed_label
+            // 
+            this.passed_label.AutoSize = true;
+            this.passed_label.Location = new System.Drawing.Point(115, 166);
+            this.passed_label.Name = "passed_label";
+            this.passed_label.Size = new System.Drawing.Size(45, 16);
+            this.passed_label.TabIndex = 3;
+            this.passed_label.Text = "Result";
+            // 
+            // calculate_btn
+            // 
+            this.calculate_btn.Location = new System.Drawing.Point(128, 245);
+            this.calculate_btn.Name = "calculate_btn";
+            this.calculate_btn.Size = new System.Drawing.Size(75, 23);
+            this.calculate_btn.TabIndex = 4;
+            this.calculate_btn.Text = "Calclulate";
+            this.calculate_btn.UseVisualStyleBackColor = true;
+            this.calculate_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // incorrect_list_label
+            // 
+            this.incorrect_list_label.AutoSize = true;
+            this.incorrect_list_label.Location = new System.Drawing.Point(118, 197);
+            this.incorrect_list_label.Name = "incorrect_list_label";
+            this.incorrect_list_label.Size = new System.Drawing.Size(10, 16);
+            this.incorrect_list_label.TabIndex = 5;
+            this.incorrect_list_label.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.incorrect_list_label);
+            this.Controls.Add(this.calculate_btn);
+            this.Controls.Add(this.passed_label);
+            this.Controls.Add(this.incorrect_label);
+            this.Controls.Add(this.correct_label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label correct_label;
+        private System.Windows.Forms.Label incorrect_label;
+        private System.Windows.Forms.Label passed_label;
+        private System.Windows.Forms.Button calculate_btn;
+        private System.Windows.Forms.Label incorrect_list_label;
+    }
+}
+
---
+++ Individual Assignment 7/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Individual_Assignment_7
+{
+    public partial class Form1 : Form
+    {
+        string[] student_answers;
+        string[] right_answers_array = new string[] {"b","d","a","a","c","a","b","a","c","d","b","c","d","a","d","c","c","b","d","a"};
+        int right_answers_number;
+        int wrong_answers_number;
+        List<string> wrong_student_answers_list;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String[] file_array = read_file();
+            student_answers = file_array;
+            reset();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            reset();
+            for (int index = 0; index < right_answers_array.Length;index++)
+            {
+                if (student_answers[index] == right_answers_array[index])
+                {
+                    right_answers_number++;
+                }
+                else
+                {
+                    wrong_answers_number++;
+                    wrong_student_answers_list.Add((index+1).ToString());
+
+                    if (!wrong_student_answers_list.Any())
+                    {
+                        incorrect_list_label.Text = "No wrong answers";
+                    }
+                    else
+                    {
+                        incorrect_list_label.Text ="Wrong answers are: "+ String.Join(", ", wrong_student_answers_list.ToArray()); ;
+                    }
+                }
+                
+            }
+        correct_label.Text = "Answered correctly: " + right_answers_number.ToString(); 
+        incorrect_label.Text = "Answered incorrectly: " + wrong_answers_number.ToString();
+
+            if (right_answers_number >= 15)
+            {
+                passed_label.Text = "You passed the exam";
+            }
+            else
+            {
+                passed_label.Text = "You failed the exam";
+            }
+            
+        }
+        private void reset()
+        {
+            String[] file_array = read_file();
+            student_answers = file_array;
+            right_answers_number = 0;
+            wrong_answers_number = 0;
+            wrong_student_answers_list = new List<string>();
+        }
+        private String[] read_file()
+        {
+            String[] numbers = new String[20];
+            int index = 0;
+            StreamReader inputFile;
+            inputFile = File.OpenText("assignment_7_student_answers.txt");
+
+            while (index<numbers.Length && !inputFile.EndOfStream)
+            {
+                numbers[index] =inputFile.ReadLine();
+                index++;
+            }
+            inputFile.Close();
+            return numbers;
+        }
+    }
+}
